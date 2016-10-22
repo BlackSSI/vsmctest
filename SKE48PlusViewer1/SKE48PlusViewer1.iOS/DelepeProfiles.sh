743009de35d7efaaa1f617805ed1c02672fbf3ca
ぶらっく <smzy.black@outlook.com>
2016-10-22 17:00:59 +0900

    change

--- SKE48PlusViewer1/SKE48PlusViewer1.iOS/DelepeProfiles.sh
+++ SKE48PlusViewer1/SKE48PlusViewer1.iOS/DelepeProfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo ----- ls 1
-ls /Users/tfsbuild/Library
-
 echo ----- ls 2
 ls /Users/tfsbuild/Library/MobileDevice
+
+echo ----- ls 3
+ls /Users/tfsbuild/Library/MobileDevice/Provisioning Profiles
 
 rm /Users/tfsbuild/Library/MobileDevice/Provisioning Profiles/*
