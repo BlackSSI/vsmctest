264d4f8e2158447a905563fb8386d13dad58d2c1
ぶらっく <smzy.black@outlook.com>
2016-12-07 00:43:13 +0900

    change script

--- SKE48PlusViewer1/SKE48PlusViewer1.iOS/DelepeProfiles.sh
+++ SKE48PlusViewer1/SKE48PlusViewer1.iOS/DelepeProfiles.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo ----- ls 2
-ls /Users/tfsbuild/Library/MobileDevice
+ls /Users/vstsbuild/Library/MobileDevice
 
 echo ----- find
-find /Users/tfsbuild -name iOSBuildTest1.* -print
+find /Users/vstsbuild -name iOSBuildTest1.* -print
 
 exit
 
 echo ----- ls 3
-ls "/Users/tfsbuild/Library/MobileDevice/Provisioning Profiles"
+ls "/Users/vstsbuild/Library/MobileDevice/Provisioning Profiles"
 
-rm /Users/tfsbuild/Library/MobileDevice/Provisioning Profiles/*
+rm /Users/vstsbuild/Library/MobileDevice/Provisioning Profiles/*
